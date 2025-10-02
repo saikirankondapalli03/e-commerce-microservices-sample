fb83a52a6e4a4645444a735e806100d03320060b
Sai Kiran Kondapalli <saikirankondapalli03@gmail.com>
2025-10-02 13:06:50 -0400

    genai artifact code repo

--- users-cna-microservice/dependencies.py
+++
@@ -1,9 +0,0 @@
-
-from db.config import async_session
-from db.dals.user_dal import UserDAL
-
-
-async def get_user_dal():
-    async with async_session() as session:
-        async with session.begin():
-            yield UserDAL(session)
